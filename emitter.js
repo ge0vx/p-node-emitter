d1e70e7982ecc612dd91a0bc3a63298cb031be61
giovany303 <giovany@303software.com>
2018-02-25 01:06:23 -0600

    biulding the object Emitte and export it

---
+++ emitter.js
@@ -0,0 +1,23 @@
+//ECMASCRIPT 5
+//creating an object by using a fucntion constructor
+function Emitter(){
+    this.events = {};
+}
+
+//methods of the emmiter object
+//the first parameter is the type of the event
+//the second parameter is the fucntion that responts to an event
+Emitter.prototype.on = function(type, listener){
+    this.events[type] = this.events[type] || [];
+    this.events[type].push(listener);
+}
+
+Emitter.prototype.emit = function(type){
+    if (this.events[type]){
+        this.events[type].forEach(function(listener){
+            listener();
+        });
+    }
+}
+
+module.exports = Emmiter;
